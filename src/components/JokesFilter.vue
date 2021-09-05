889173429b564944f3ffe34eefbe53d9e7766d41
Anton Tigunov <seyanaracore@gmail.com>
2021-09-05 16:45:29 +0300

    filter comp

--- src/components/JokesFilter.vue
+++ src/components/JokesFilter.vue
@@ -1,12 +1,37 @@
- <template>
-  <div></div>
+<template>
+  <form class="col-12 col-sm-10 col-lg-6" @submit.prevent>
+    <input
+      v-model="filterWords"
+      type="text"
+      class="form-control"
+      placeholder="Введите слово для поиска среди анекдотов..."
+    />
+  </form>
 </template>
- 
- <script>
+
+<script>
 export default {
-  name: "JokesFilter",
+  props: ["jokesList"],
+
+  data() {
+    return {
+      filterWords: "",
+    };
+  },
+
+  watch: {
+    filterWords() {
+      const { filterWords, jokesList } = this;
+      let filteredJokes = jokesList.filter((joke) => {
+        let props = ["setup", "delivery", "joke"];
+        let jokeText = "";
+        if (joke[props[0]]) jokeText += joke[props[0]];
+        if (joke[props[1]]) jokeText += joke[props[1]];
+        if (joke[props[2]]) jokeText += joke[props[2]];
+        return jokeText.toLowerCase().includes(filterWords.toLowerCase());
+      });
+      this.$emit("filterJokes", filteredJokes);
+    },
+  },
 };
 </script>
- 
- <style>
-</style>
