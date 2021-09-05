7953af12e3422737acbcc5dd6d5966156f44481f
Anton Tigunov <seyanaracore@gmail.com>
2021-09-05 15:59:47 +0300

    jokesList comp

--- src/components/JokesList.vue
+++ src/components/JokesList.vue
@@ -1,12 +1,44 @@
- <template>
-  <div></div>
+<template>
+  <div class="row">
+    <div class="col-12 col-sm-10 col-lg-6">
+      <ul class="list-group">
+        <joke
+          v-for="(joke, index) in filteredJokesList"
+          :key="index"
+          @likeIt="likeIt"
+          :joke="joke"
+        />
+      </ul>
+    </div>
+  </div>
 </template>
- 
- <script>
+
+<script>
+import Joke from "./Joke.vue";
+
 export default {
-  name: "JokesList",
+  props: {
+    jokesList: {
+      type: Array,
+      default: () => [],
+    },
+    filteredJokesList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  methods: {
+    likeIt(filteredJokeId) {
+      this.jokesList.forEach((joke) => {
+        if (joke.id !== filteredJokeId) return;
+        joke.liked === undefined
+          ? this.$set(joke, "liked", true)
+          : (joke.liked = !joke.liked);
+      });
+    },
+  },
+
+  components: { Joke },
 };
 </script>
- 
- <style>
-</style>
