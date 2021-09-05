95e46e4d06ab8d4919b31cb3071a555bcdc9404f
Anton Tigunov <seyanaracore@gmail.com>
2021-09-05 15:51:27 +0300

    Jokes comp

--- src/components/Jokes.vue
+++ src/components/Jokes.vue
@@ -1,12 +1,51 @@
- <template>
-  <div></div>
+<template>
+  <div class="container">
+    <div class="row mb-5">
+      <div class="col-12 col-sm-10 col-lg-6">
+        <h1>{{ listName }}</h1>
+      </div>
+    </div>
+    <div class="row mb-3">
+      <jokes-filter :jokesList="jokesList" @filterJokes="setFilteredJokes" />
+    </div>
+    <jokes-list
+      :jokesList="jokesList"
+      :filteredJokesList="filteredJokes ? filteredJokes : jokesList"
+    />
+  </div>
 </template>
- 
- <script>
+
+<script>
+import JokesList from "./JokesList.vue";
+import JokesFilter from "./JokesFilter.vue";
+
 export default {
-  name: "Jokes",
+  components: { JokesList, JokesFilter },
+
+  props: {
+    listName: String,
+    jokesList: {
+      type: Array,
+      defult: () => [],
+    },
+  },
+
+  data() {
+    return {
+      filteredJokes: null,
+    };
+  },
+
+  methods: {
+    setFilteredJokes(filteredJokes) {
+      this.filteredJokes = filteredJokes;
+    },
+  },
 };
 </script>
- 
- <style>
+
+<style>
+.row {
+  justify-content: center;
+}
 </style>
